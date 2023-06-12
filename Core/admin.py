7877807187bfd23d09d5e3242e5d2ef78878d880
Reza Rohani <theonlykingpin@gmail.com>
2023-06-12 14:04:30 +0330

    Update admin.py

--- Core/admin.py
+++ Core/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Topic, Entry
 
 
-admin.site.register(Topic)
-admin.site.register(Entry)
+@admin.register(Topic)
+class TopicsAdmin(admin.modeladmin):
+    pass
+
+
+@admin.register(Entry)
+class EntriesAdmin(admin.modeladmin):
+    pass
