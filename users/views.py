4bcca1a57b4d505079a99483d8d2845b210d3d36
Reza Rohani <theonlykingpin@gmail.com>
2025-03-09 16:58:05 +0330

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,15 +2,15 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-
 def register(request):
-    if request.method != 'POST':
-        form = UserCreationForm()
-    else:
+    if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
             login(request, new_user)
             return redirect('core:index')
+    else:
+        form = UserCreationForm()
+    
     context = {'form': form}
     return render(request, 'registration/register.html', context)
